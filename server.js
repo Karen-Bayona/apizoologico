925c8c9f26ede9c2ebcc935769ce1917f2b9c5a4
Karen <karenmbayonar@gmail.com>
2026-04-09 19:57:39 -0700

    Parte clase 04/09

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const authRoutes = require("./routes/authentication");
 const app = express();
 
 // Middleware
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Rutas
 app.use("/api/animals", require("./routes/animalRoutes"));
-
+app.use("/api",authRoutes);
 // Ruta base
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
